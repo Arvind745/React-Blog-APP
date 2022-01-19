63bad18ce2d4697f6c81dc3e2806498624309faa
Arvind745 <malviyaarvind232323@gmail.com>
2022-01-20 01:01:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Bollywood from './Bollywood';
+import Travel from './Travel';
+import Hollywood from './Hollywood';
+import Technology from './Technology';
+import Fitness from './Fitness';
+
+const App = () => {
+  return(
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/bollywood" element={<Bollywood />}></Route>
+        <Route path="/technology" element={<Technology />}></Route>
+        <Route path="/Hollywood" element={<Hollywood />}></Route>
+        <Route path="/fitness" element={<Fitness />}></Route>
+        <Route path="/travel" element={<Travel />}></Route>
+      </Routes>
+     </BrowserRouter>
+    </>
+    
+  )
 }
+
 
 export default App;
